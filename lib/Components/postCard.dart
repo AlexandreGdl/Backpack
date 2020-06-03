c4fb6eb8b094cffb273b11c446e7f2aae04e1ef0
AlexandreGdl <guindeuilalexandre@gmail.com>
2020-06-04 00:22:50 +0200

    🔧 fix (pubsmec): adding font and images file

---
+++ lib/Components/postCard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[],
+          )
+        ],
+      )
+    );
+  }
+}
